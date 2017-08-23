769df4266e45e9aa7905740f2aeaee3be61bb484
Kathy <kchen2013@gmail.com>
2017-08-23 12:29:38 -0400

    refactoring splicingdata class for my specific use case (identifying features in coding/noncoding regions)

--- seqmodel.py
+++ seqmodel.py
@@ -185,7 +185,7 @@ class SplicingDataset:
         features : list[str]
             List of genomic features.
         holdout_chrs : list[str]
-            Chromosomes that act as our holdout dataset.
+            Chromosomes that act as our holdout (test) dataset.
         radius : int, optional
             Default is 100.
         mode : {"all", "train", "test"}, optional
@@ -200,11 +200,15 @@ class SplicingDataset:
         mode : {"all", "train", "test"}
 
         """
+        MODES = ["all", "train", "test"]
+        if mode not in MODES:
+            raise ValueError("Mode must be one of {0}. Input was '{1}'.".format(MODES, mode))
+        self.set_mode(mode)
+
         self.genome = Genome(genome)
         self.genome_features = GenomicData(genome_data, features)
         self.holdout_chrs = holdout_chrs
         self.radius = radius
-        self.mode = mode
 
         #self.genome = Genome(os.path.join(DIR, "hg38.fa"))
         #self.data = GenomicData(os.path.join(DIR, "splicejunc.database.bed.sorted.gz"),nfeatures=2,featureDict={'5p':0, '3p':1})
@@ -221,14 +225,15 @@ class SplicingDataset:
         #self.geneanno['p'] = (self.geneanno['end_shifted'] - self.geneanno['start_shifted']+1)/np.sum(self.geneanno['end_shifted'] - self.geneanno['start_shifted']+self.geneanno.shape[0])
         #self.geneanno['p'] = np.asarray(self.geneanno['p'] * (self.geneanno['p']>=0))/np.sum(self.geneanno['p'] * (self.geneanno['p']>=0))
 
-        if mode == 'all':
-            self.all_mode()
-        elif mode == 'train':
-            self.train_mode(mode='train')
-        elif mode == 'test':
-            self.train_mode(mode='test')
-        else:
-            raise ValueError('Mode has to be one of "all", "train", and "test".')
+
+        #if mode == "all":
+        #    self.all_mode()
+        #elif mode == 'train':
+        #    self.train_mode(mode='train')
+        #elif mode == 'test':
+        #    self.train_mode(mode='test')
+        #else:
+        #    raise ValueError('Mode has to be one of "all", "train", and "test".')
 
     def _retrieve(self, chrom, position, strand, radius,
                   sequence_only=False, verbose=False, padding=(0, 0)):
@@ -268,29 +273,44 @@ class SplicingDataset:
                 chrom, pos - radius, pos + radius + 1, strand, verbose=verbose)
             return (retrieved_sequence, retrieved_data)
 
+"""
     def sample_background(self, sequence_only=False, verbose=False, padding=[0,0]):
-        #select random gene weighted by gene length
+        # TODO: documentation
+        # TODO: random seed
         if len(self._randcache)==0:
-            self._randcache = list(np.random.choice(self._inds, p=self._genep , size=2000))
+            self._randcache = list(np.random.choice(self._inds, p=self._genep, size=2000))
         randind = self._randcache.pop()
 
-        chr = self._genechr[randind]
-        pos = int(self._genestart[randind] + np.random.uniform()*(self._geneend[randind]-self._genestart[randind]))
+        gene_length = self._geneend[randind] - self._genestart[randind]
+        chrom = self._genechr[randind]
+        position = int(self._genestart[randind] + np.random.uniform() * gene_length
         strand = self._genestrand[randind]
 
-
-        return self._retrieve(chr, pos, strand, self.radius, sequence_only, verbose, padding=padding)
+        return self._retrieve(chrom, position, strand, self.radius,
+                              sequence_only=sequence_only,
+                              verbose=verbose,
+                              padding=padding)
 
 
     def sample_positive(self, sequence_only=False, verbose=False, padding=[0,0]):
         randind = np.random.randint(0,self._poschr.shape[0])
-        chr = self._poschr[randind]
-        pos = int(self._posstart[randind] + np.random.uniform()*(self._posend[randind]-self._posstart[randind]) + np.random.uniform()*self.radius)
+
+        gene_length = self._posend[randind] - self._posstart[randind]
+        chrom = self._poschr[randind]
+
+        rand_in_gene = np.random.uniform() * gene_length
+        rand_in_radius = np.random.uniform() * self.radius
+        position = int(
+            self._posstart[randind] + rand_in_gene + rand_in_radius)
+
         strand = self._posstrand[randind]
 
         if verbose:
-            print chr, pos, strand
-        return self._retrieve(chr, pos, strand, self.radius, sequence_only, verbose, padding=padding)
+            print chrom, position, strand
+        return self._retrieve(chrom, position, strand, self.radius,
+                              sequence_only=sequence_only,
+                              verbose=verbose,
+                              padding=padding)
 
     def sample_mixture(self, positive_prop=0.5, sequence_only=False, verbose=False, padding=[0,0]):
         if np.random.uniform() < positive_prop:
@@ -333,85 +353,57 @@ class SplicingDataset:
         self._posstart=np.asarray(self.positives.iloc[:,1])[pind]
         self._posend=np.asarray(self.positives.iloc[:,2])[pind]
         self._randcache=[]
-
-
-
-# from matplotlib import pyplot as plt
-# %matplotlib inline
-
-#temporary fix
-def _patched_view_4d(*tensors):
-    output = []
-    for t in tensors:
-        assert t.dim() == 3
-        size = list(t.size())
-        size.insert(2, 1)
-        output += [t.contiguous().view(*size)]
-    return output
-
-#import torch.nn._functions.conv
-#torch.nn._functions.conv._view4d = _patched_view_4d
-
-def clip_gradient(model, clip):
-    """Computes a gradient clipping coefficient based on gradient norm."""
-    totalnorm = 0
-    for p in model.parameters():
-        modulenorm = p.grad.data.norm()
-        totalnorm += modulenorm ** 2
-    totalnorm = math.sqrt(totalnorm)
-
-    ratio = min(1, clip / (totalnorm + 1e-6))
-    for p in model.parameters():
-        p.grad.mul_(ratio)
-    return min(1, clip / (totalnorm + 1e-6))
-
-
-#model specification
+"""
 
 hiddenSizes = [100,2]
-
 n_lstm_layers = 2
-rnn = nn.LSTM(input_size =  4, hidden_size = hiddenSizes[0], num_layers=n_lstm_layers, batch_first =True, bidirectional=True)
+rnn = nn.LSTM(input_size=4, hidden_size=hiddenSizes[0], num_layers=n_lstm_layers, batch_first=True, bidirectional=True)
 
 conv = nn.modules.container.Sequential(
     nn.Conv1d(hiddenSizes[0]*2, hiddenSizes[0]*2, 1),
     nn.ReLU(),
     nn.Conv1d(hiddenSizes[0]*2, hiddenSizes[1], 1))
 
-model = [ rnn, conv]
+model = [rnn, conv]
 useCuda = True
 if useCuda:
     for module in model:
         module.cuda()
 
 optimizers = [optim.SGD(module.parameters(), lr = 0.05, momentum=0.95) for module in model]
-#optimizers = [optim.Adam(module.parameters(), lr = 0.1) for module in model]
 criterion = nn.MSELoss()
 
-sdata = SplicingDataset(mode='train',radius=100)
+sdata = SplicingDataset(
+    os.path.join(DIR, "hg38.fa"),
+    os.path.join(DIR, "splicejunc.database.bed.sorted.gz"),
+    ["5p", "3p"],
+    ["chr8", "chr9"],
+    radius=100,
+    mode="train")
 
-padding = [0,0]
-def runBatch(batchSize= 16, update=True, plot=False):
-    inputs = np.zeros((batchSize, sdata.radius*2+1+padding[0]+padding[1], 4))
-    targets = np.zeros((batchSize, sdata.radius*2+1, 2))
+padding = (0, 0)
+
+
+def runBatch(batchSize=16, update=True, plot=False):
+    region_len = sdata.radius * 2 + 1 + sum(padding)
+    inputs = np.zeros((batchSize, region_len, len(BASES)))
+    targets = np.zeros((batchSize, sdata.radius * 2 + 1, 2))
     for i in range(batchSize):
-        input, target = sdata.sample_mixture(0.5,padding=padding)
-        inputs[i,:,:]=input
-        targets[i,:,:]= np.log10(target+1e-6)+6
-        #targets[targets<8]=0
-    #temporary fix
-
+        sequence, target = sdata.sample_mixture(0.5, padding=padding)
+        inputs[i, :, :] = sequence
+        #targets[i,:,:] = np.log10(target+1e-6)+6
+        targets[i, :, :] = target
 
     if useCuda:
-        inputs = Variable(torch.Tensor(inputs).cuda(),requires_grad=True)
+        inputs = Variable(torch.Tensor(inputs).cuda(), requires_grad=True)
         targets = Variable(torch.Tensor(targets).cuda())
         h0 = Variable(torch.zeros(n_lstm_layers*2, batchSize, hiddenSizes[0]).cuda())
         c0 = Variable(torch.zeros(n_lstm_layers*2, batchSize, hiddenSizes[0]).cuda())
     else:
-        inputs = Variable(torch.Tensor(inputs),requires_grad=True)
+        inputs = Variable(torch.Tensor(inputs), requires_grad=True)
         targets = Variable(torch.Tensor(targets))
-        h0 = Variable(torch.zeros(n_lstm_layers*2, batchSize, hiddenSizes[0]))
-        c0 = Variable(torch.zeros(n_lstm_layers*2, batchSize, hiddenSizes[0]))
+        h0 = Variable(torch.zeros(n_lstm_layers * 2, batchSize, hiddenSizes[0]))
+        c0 = Variable(torch.zeros(n_lstm_layers * 2, batchSize, hiddenSizes[0]))
 
     outputs, hn = rnn(inputs, (h0, c0))
     outputs = conv(outputs.transpose(1,2)).transpose(1,2)
@@ -426,10 +418,6 @@ def runBatch(batchSize= 16, update=True, plot=False):
             optimizer.step()
 
     if plot:
-        # plt.figure()
-        # plt.plot(outputs.data[0,:,:].numpy(),'r')
-        # plt.plot(targets.data[0,:,:].numpy(),'g')
-        # plt.show()
         plt.figure()
         plt.plot(outputs.data.numpy().flatten(),targets.data.numpy().flatten(),'.',alpha=0.2)
         plt.show()
@@ -438,22 +426,15 @@ def runBatch(batchSize= 16, update=True, plot=False):
 
 for _ in range(10000):
     sdata.train_mode()
-    cumlossTrain=0
+    cumlossTrain = 0
     for _ in range(1000):
         cumlossTrain = cumlossTrain + runBatch()
 
     sdata.train_mode('test')
-    cumlossTest=0
+    cumlossTest = 0
     for _ in range(100):
         cumlossTest = cumlossTest + runBatch(update=False)
     print("Train loss: %.5f, Test loss: %.5f." % (cumlossTrain, cumlossTest) )
 
 
 torch.save(model,os.path.join(DIR, "models/101bp.h100.cpu.model"))
-#model_trained = torch.load('/Genomics/ogtr04/jzthree/splicing/models/201bp.h100.cpu.model')
-#sdata.radius=100
-
-#model_trained = torch.load('/Genomics/ogtr04/jzthree/splicing/models/201bp.h500.posonly.cpu.model')
-#sdata.radius=100
-#sdata.train_mode('test')
-#runBatch(batchSize=1024, update=False,  plot=True)
