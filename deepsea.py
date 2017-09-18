e579fb9d30b59aa910c491644d503bbd0bba4c0a
Kathy Chen <kchen2013@gmail.com>
2017-09-18 10:47:43 -0400

    updated deepsea to have window size 8 and forward to have renorm function

--- deepsea.py
+++ deepsea.py
@@ -22,22 +22,22 @@ class DeepSEA(nn.Module):
         """
         super(DeepSEA, self).__init__()
         self.conv_net = nn.Sequential(
-            nn.Conv1d(4, 320, 1),
+            nn.Conv1d(4, 320, 8),
             nn.ReLU(inplace=True),
             nn.MaxPool1d(4),
             nn.Dropout(p=0.2),
 
-            nn.Conv1d(320, 480, 1),
+            nn.Conv1d(320, 480, 8),
             nn.ReLU(inplace=True),
             nn.MaxPool1d(4),
             nn.Dropout(p=0.2),
 
-            nn.Conv1d(480, 960, 1),
+            nn.Conv1d(480, 960, 8),
             nn.ReLU(inplace=True),
             nn.Dropout(p=0.5))
 
-        self.n_channels = int(np.floor(np.floor(
-            window_size / 4.) / 4.))
+        self.n_channels = int((np.floor(np.floor(
+            (window_size - 7) / 4.) - 7) / 4.)) - 7
 
         self.classifier = nn.Sequential(
             nn.Linear(960 * self.n_channels, n_genomic_features),
@@ -49,6 +49,12 @@ class DeepSEA(nn.Module):
     def forward(self, x):
         """Forward propagation of a batch.
         """
+        for layer in self.conv_net.children():
+            if isinstance(layer, nn.Conv1d):
+                layer.weight.renorm(2, 1, 0.9)
+        for layer in self.classifier.children():
+            if isinstance(layer, nn.Linear):
+                layer.weight.renorm(2, 1, 0.9)
         x = self.conv_net(x)
         x = x.view(x.size(0), 960 * self.n_channels)
         x = self.classifier(x)
