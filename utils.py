b2f59c828f227c1fb198f6136fe598410ec3f1a1
Kathy Chen <kchen2013@gmail.com>
2017-11-02 12:38:24 -0400

    fixed a bug in yaml loading utils function (forgot the return statement)

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ def read_yaml_file(config_file):
     with open(config_file, "r") as config_file:
         try:
             config_dict = yaml.load(config_file)
+            return config_dict
         except yaml.YAMLError as expection:
             sys.exit(exception)
 
