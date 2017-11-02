beb6a4d829d5c4aba899e203f9b746bcb7cdd0f1
Kathy Chen <kchen2013@gmail.com>
2017-11-02 13:55:50 -0400

    updating my SLURM runner script with the new train model args

--- runscript.sh
+++ runscript.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH --time=02:00:00
-#SBATCH --mem=50000
+#SBATCH --time=96:00:00
+#SBATCH --mem=75000
 #SBATCH --gres=gpu:1
-#SBATCH -o o_seqmodel_tgpu_%j.out
-#SBATCH -e e_seqmodel_tgpu_%j.err
+#SBATCH -o o_mouse_%j.out
+#SBATCH -e e_mouse_%j.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=kchen@flatironinstitute.org
 
 module load cudnn/cuda-8.0/6.0
 module load anaconda3/4.4.0
 
-export myscratch="/scratch/kc31/rf-job"
-data_dir="/tigress/kc31/data_small"
-
-rm -rf $myscratch
-mkdir -p $myscratch
+hg19="/tigress/kc31/hg19/*"
+data_dir_original="/tigress/kc31/hg_188ChIPfeats/*"
+export data_dir="/scratch/data_hg"
 
 source activate mazu
 
-python -u train_model.py $data_dir"/mm10_no_alt_analysis_set_ENCODE.fasta" \
-                    $data_dir"/reduced_agg_beds_1.bed" \
-                    $data_dir"/reduced_agg_beds_1.bed.gz" \
-                    $myscratch"/output91017.model" \
-                    --holdout-chrs=chr8,chr9 --radius=100 --window=1001 \
-                    --random-seed=123 --mode=train --use-cuda
+rm -rf $data_dir
+mkdir -p $data_dir
+cp -r $data_dir_original $data_dir
+cp -r $hg19 $data_dir
 
-cp -r $myscratch /tigress/kc31/
-rm -rf $myscratch
+python -u train_model.py deepsea SGD 0.005 \
+                         configs/paths.yml \
+                         configs/train_model.yml \
+                         --runs=1 --verbose
+rm -rf $data_dir
