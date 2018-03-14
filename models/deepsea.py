cfc912e99f6cbae957e5b8b7417e6c4c4b7eb2e9
Kathy Chen <kchen2013@gmail.com>
2018-03-14 10:12:13 -0400

    add a models dir with deepsea in it

---
+++ models/deepsea.py
@@ -0,0 +1,79 @@
+"""DeepSEA architecture (Zhou & Troyanskaya, 2015).
+"""
+import numpy as np
+import torch
+import torch.nn as nn
+
+
+class DeepSEA(nn.Module):
+    def __init__(self, window_size, n_genomic_features):
+        """
+        Parameters
+        ----------
+        window_size : int
+        n_genomic_features : int
+
+        Attributes
+        ----------
+        conv_net : torch.nn.Sequential
+        n_channels : int
+        classifier : torch.nn.Sequential
+        """
+        super(DeepSEA, self).__init__()
+        conv_kernel_size = 8
+        pool_kernel_size = 4
+
+        self.conv_net = nn.Sequential(
+            nn.Conv1d(4, 320, kernel_size=conv_kernel_size),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(
+                kernel_size=pool_kernel_size, stride=pool_kernel_size),
+            nn.BatchNorm1d(320),
+
+            nn.Conv1d(320, 480, kernel_size=conv_kernel_size),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(
+                kernel_size=pool_kernel_size, stride=pool_kernel_size),
+            nn.BatchNorm1d(480),
+            nn.Dropout(p=0.2),
+
+            nn.Conv1d(480, 960, kernel_size=conv_kernel_size),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm1d(960),
+            nn.Dropout(p=0.5))
+
+        reduce_by = conv_kernel_size - 1
+        pool_kernel_size = float(pool_kernel_size)
+        self.n_channels = int(
+            np.floor(
+                (np.floor(
+                    (window_size - reduce_by) / pool_kernel_size)
+                 - reduce_by) / pool_kernel_size)
+            - reduce_by)
+        self.classifier = nn.Sequential(
+            nn.Linear(960 * self.n_channels, n_genomic_features),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm1d(n_genomic_features),
+            nn.Linear(n_genomic_features, n_genomic_features),
+            nn.Sigmoid())
+
+    def forward(self, x):
+        """Forward propagation of a batch.
+        """
+        for layer in self.conv_net.children():
+            if isinstance(layer, nn.Conv1d):
+                layer.weight.data.renorm_(2, 0, 0.9)
+        for layer in self.classifier.children():
+            if isinstance(layer, nn.Linear):
+                layer.weight.data.renorm_(2, 0, 0.9)
+        out = self.conv_net(x)
+        reshape_out = out.view(out.size(0), 960 * self.n_channels)
+        predict = self.classifier(reshape_out)
+        return predict
+
+def criterion():
+    return nn.BCELoss()
+
+def get_optimizer(lr):
+    return (torch.optim.SGD,
+            {"lr": lr, "weight_decay": 1e-6, "momentum": 0.9})
