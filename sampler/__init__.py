24bf50ee4740014f8c7a709822468b86b72cecdd
Kathy Chen <kchen2013@gmail.com>
2018-03-22 18:24:46 -0400

    adding a new sampler module that will replace sampler.py entirely

---
+++ sampler/__init__.py
@@ -0,0 +1,3 @@
+from .sampler import Sampler
+from .intervals_sampler import IntervalsSampler
+
---
+++ sampler/sampler.py
@@ -0,0 +1,111 @@
+import random
+
+import numpy as np
+
+from data_utils import Genome
+from data_utils import GenomicFeatures
+
+
+class Sampler(object):
+
+    STRAND_SIDES = ('+', '-')
+
+    def __init__(self,
+                 genome,
+                 query_feature_data,
+                 distinct_features,
+                 random_seed=436,
+                 validation_holdout=['6', '7'],
+                 test_holdout=['8', '9'],
+                 sequence_length=1001,
+                 center_bin_to_predict=201,
+                 feature_thresholds=0.5,
+                 mode="train"):
+        # @TODO: do sequence len and center bin len need to both
+        # be odd numbers? Should I make this more flexible?
+        if sequence_length % 2 == 0 or center_bin_to_predict % 2 == 0:
+            raise ValueError(
+                "Both the sequence length and the center bin length "
+                "should be odd numbers. Sequence length was {0} and "
+                "bin length was {1}.".format(
+                    sequence_length, center_bin_to_predict))
+
+        surrounding_sequence_length = \
+            sequence_length - center_bin_to_predict
+        if surrounding_sequence_length < 0:
+            raise ValueError(
+                "Sequence length of {0} is less than the center bin "
+                "length of {1}.".format(
+                    sequence_length, center_bin_to_predict))
+
+        self.modes = ["train", "validate"]
+        if test_holdout:
+            self.modes.append("test")
+            if type(validation_holdout) == type(list()) and \
+                    type(test_holdout) == type(list()):
+                self.validation_holdout = [
+                    str(c) for c in validation_holdout]
+                self.test_holdout = [str(c) for c in test_holdout]
+                self._holdout_type = "chromosome"
+            elif isinstance(validation_holdout, float) and \
+                    isinstance(test_holdout, float):
+                self.validation_holdout = validation_holdout
+                self.test_holdout = test_holdout
+                self._holdout_type = "proportion"
+            else:
+                raise ValueError(
+                    "Validation holdout and test holdout must have the "
+                    "same type (list or float) but validation was "
+                    "type {0} and test was type {1}".format(
+                        type(validation_holdout), type(test_holdout)))
+        else:
+            self.test_holdout = None
+            if type(validation_holdout) == type(list()):
+                self.validation_holdout = [
+                    str(c) for c in validation_holdout]
+            else:
+                self.validation_holdout = validation_holdout
+
+        if mode not in self.modes:
+            raise ValueError(
+                "Mode must be one of {0}. Input was '{1}'.".format(
+                    self.modes, mode))
+        self.mode = mode
+
+        self.surrounding_sequence_radius = int(
+            surrounding_sequence_length / 2)
+        self.sequence_length = sequence_length
+        self.bin_radius = int((center_bin_to_predict - 1) / 2)
+
+        np.random.seed(random_seed)
+        random.seed(random_seed + 1)
+        self.random_seed = random_seed
+
+        self.genome = Genome(genome)
+
+        self._features = []
+        with open(distinct_features, 'r') as file_handle:
+            for line in file_handle:
+                self._features.append(line.strip())
+        self.n_features = len(self._features)
+
+        self.query_feature_data = GenomicFeatures(
+            query_feature_data, self._features,
+            feature_thresholds=feature_thresholds)
+
+    def get_feature_from_index(self, feature_index):
+        """Returns the feature corresponding to an index in the feature
+        vector. Currently used by the 'deepsea' logger.
+
+        Parameters
+        ----------
+        feature_index : int
+
+        Returns
+        -------
+        str
+        """
+        return self.query_feature_data.index_feature_map[feature_index]
+
+    def sample(self, batch_size):
+        raise NotImplementedError
