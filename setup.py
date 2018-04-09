e842badc711e439a0aed11402dad66c02037bbd7
Kathy Chen <kchen2013@gmail.com>
2018-04-09 11:38:01 -0400

    separated fastloop.pyx into 2 diff files, updated the setup.py for it

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
+import numpy as np
 from Cython.Build import cythonize
 
-setup(name="data_utils.fastloop", ext_modules=cythonize('data_utils/fastloop.pyx'),)
+genome_module = Extension(
+    "selene.sequences._genome",
+    ["selene/sequences/_genome.pyx"],
+    include_dirs=[np.get_include()])
+
+genomic_features_module = Extension(
+    "selene.targets._genomic_features",
+    ["selene/targets/_genomic_features.pyx"],
+    include_dirs=[np.get_include()])
+
+ext_modules = [genome_module, genomic_features_module]
+
+setup(name="selene",
+      version="0.0.0",
+      description="framework for training sequence-level " \
+                  "deep learning networks",
+      packages=["selene"],
+      ext_modules=cythonize(ext_modules))
