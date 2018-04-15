225d889c6bdc87a852c933b5c0642577955708ff
Kathy <kchen2013@gmail.com>
2018-04-15 13:02:47 -0400

    starting on functions in model predict

--- selene/utils/utils.py
+++ selene/utils/utils.py
@@ -4,6 +4,9 @@ import sys
 import yaml
 
 
+VCF_REQUIRED_COLS = ["#CHROM", "POS", "ID", "REF", "ALT"]
+
+
 def initialize_logger(out_filepath, verbosity=1, stdout_handler=False):
     """This function can only be called successfully once.
     If the logger has already been initialized with handlers,
@@ -41,3 +44,30 @@ def read_yaml_file(config_file):
             return config_dict
         except yaml.YAMLError as exception:
             sys.exit(exception)
+
+
+def read_vcf_file(vcf_file):
+    """Read the relevant columns for a VCF file to collect variants
+    for variant effect prediction.
+    """
+    variants = []
+    with open(vcf_file, 'r') as file_handle:
+        for line in file_handle:
+            if "#CHROM" in line:
+                cols = line.strip().split('\t')
+                if cols[:5] != VCF_REQUIRED_COLS:
+                    raise ValueError(
+                        "First 5 columns in file {0} were {1}. "
+                        "Expected columns: {2}".format(
+                            vcf_file, cols[:5], VCF_REQUIRED_COLS))
+                break
+
+        for line in file_handle:
+            cols = line.strip().split('\t')
+            chrom = str(cols[0])
+            pos = int(cols[1])
+            ref = cols[3]
+            alt = cols[4]
+            variants.append((chrom, pos, ref, alt))
+    return variants
+
