8d2ccfbdcf532af03021e4b510bb4cba81f05345
evancofer <evancofer@gmail.com>
2018-04-19 22:03:11 -0400

    Added plotting of in silico mutagenesis results as a heatmap

--- selene/utils/utils.py
+++ selene/utils/utils.py
@@ -1,7 +1,9 @@
 import logging
 import sys
-
+import seaborn as sns
 import yaml
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def initialize_logger(out_filepath, verbosity=1, stdout_handler=False):
@@ -32,6 +34,26 @@ def initialize_logger(out_filepath, verbosity=1, stdout_handler=False):
         stream_handle = logging.StreamHandler(sys.stdout)
         stream_handle.setFormatter(formatter)
         logger.addHandler(stream_handle)
+
+
+def get_in_silico_mutagenesis_heatmap(ref_seq, ref_pred, mut_seqs, mut_preds, genome):
+    """
+    Turns the results of an in silico saturated mutagenesis into a matrix, where each row is a base
+    and each column is a position in the sequence. The value is the prediction value when that position
+    in the sequence has been set to that base.
+    :param ref_seq: Reference sequence.
+    :param ref_pred: Prediction for reference sequence.
+    :param mut_seqs: Mutant sequences.
+    :param mut_preds: Predictions for mutant sequences.
+    :param genome: A genome or other reference sequence.
+    :return: n*m matrix of predictions by base change.
+    """
+    ref_enc = genome.sequence_to_encoding(ref_seq)
+    ret = ref_enc * ref_pred
+    for mut_seq, mut_pred in zip(mut_seqs, mut_preds):
+        mut_enc = genome.sequence_to_encoding(mut_seq)
+        ret += ((mut_enc - ref_enc) * mut_pred)
+    return sns.heatmap(ret, linewidths=0, yticklabels=genome.BASES_ARR, cmap="RdBu_r")
 
 
 def read_yaml_file(config_file):
