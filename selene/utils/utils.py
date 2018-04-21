a45610d992e73def90f9731d78e0bd060f018afa
Kathy <kchen2013@gmail.com>
2018-04-21 19:20:13 -0400

    remove the yaml file reading from utils

--- selene/utils/utils.py
+++ selene/utils/utils.py
@@ -1,13 +1,10 @@
 import logging
-import sys
-
-import yaml
 
 
 VCF_REQUIRED_COLS = ["#CHROM", "POS", "ID", "REF", "ALT"]
 
 
-def initialize_logger(out_filepath, verbosity=1, stdout_handler=False):
+def initialize_logger(out_filepath, verbosity=2):
     """This function can only be called successfully once.
     If the logger has already been initialized with handlers,
     the function exits. Otherwise, it proceeds to set the
@@ -30,20 +27,6 @@ def initialize_logger(out_filepath, verbosity=1, stdout_handler=False):
     file_handle = logging.FileHandler(out_filepath)
     file_handle.setFormatter(formatter)
     logger.addHandler(file_handle)
-
-    if stdout_handler:
-        stream_handle = logging.StreamHandler(sys.stdout)
-        stream_handle.setFormatter(formatter)
-        logger.addHandler(stream_handle)
-
-
-def read_yaml_file(config_file):
-    with open(config_file, "r") as config_file:
-        try:
-            config_dict = yaml.load(config_file)
-            return config_dict
-        except yaml.YAMLError as exception:
-            sys.exit(exception)
 
 
 def read_vcf_file(vcf_file):
