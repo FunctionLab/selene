27dba1bc8d42370bcf5a0722d06eb765ede9444b
evancofer <evancofer@gmail.com>
2018-05-03 19:41:01 -0400

    Changed name of logo visualization method

--- selene/utils/__init__.py
+++ selene/utils/__init__.py
@@ -1,5 +1,5 @@
 from .utils import initialize_logger
 from .performance_metrics import PerformanceMetrics
 from .config import load, load_path, instantiate
-from .vis import get_matrix_from_in_silico_mutagenesis_results, get_heatmap_from_in_silico_mutagenesis_results, get_logo_from_in_silico_mutagenesis_results
+from .vis import get_matrix_from_in_silico_mutagenesis_results, get_heatmap_from_in_silico_mutagenesis_results, sequence_logo
 
