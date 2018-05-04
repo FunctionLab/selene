ec3582e7866e377cab34a3cb3a92787e327db2d1
Kathy <kchen2013@gmail.com>
2018-05-04 14:36:24 -0400

    CLI starting to handle diff operations

--- selene.py
+++ selene.py
@@ -6,90 +6,113 @@ Output:
     Saves model to a user-specified output file.
 
 Usage:
-    selene.py <lr> <config-yml>
+    selene.py <config-yml> [--lr=<lr>]
     selene.py -h | --help
 
 Options:
     -h --help               Show this screen.
 
-    <lr>                    Choose the optimizer's learning rate
     <config-yml>            Model-specific parameters
+    --lr=<lr>               If training, the optimizer's learning rate
+                            [default: None]
 """
-import importlib
+import importlib.util
 
 from docopt import docopt
 import torch
 
 from selene.utils import load_path, instantiate
 
+
+def initialize_model(model_configs, train=True, lr=None):
+    import_model_from = model_configs["file"]
+    model_class_name = model_configs["class"]
+
+    spec = importlib.util.spec_from_file_location("module", import_model_from)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    model_class = getattr(module, model_class_name)
+
+    if train:
+        criterion = module.criterion()
+        optim_class, optim_args = module.get_optimizer(lr)
+
+    sequence_length = model_configs["sequence_length"]
+    n_classes = model_configs["n_classes_to_predict"]
+    model = model_class(sequence_length, n_classes)
+
+    if model_configs["non_strand_specific"]["use_module"]:
+        from models.non_strand_specific_module import NonStrandSpecific
+        model = NonStrandSpecific(
+            model, mode=model_configs["non_strand_specific"]["mode"])
+    if train:
+        return model, criterion, optim_class, optim_args
+    else:
+        return model
+
+def execute(operation, config, model=None):
+    if operation == "train":
+        model, loss, optim, optim_args = initialize_model(
+            config["model"], train=True, lr=config["lr"])
+
+        sampler_info = configs["sampler"]
+        train_model_info = configs["train_model"]
+
+        data_sampler = instantiate(sampler_info)
+
+        train_model_info.bind(
+            model=model,
+            data_sampler=data_sampler,
+            loss_criterion=loss,
+            optimizer_class=optim,
+            optimizer_args=optim_args)
+
+        trainer = instantiate(train_model_info)
+        trainer.train_and_validate()
+        if "evaluate_on_test" in config and config["evaluate_on_test"]:
+            trainer.evaluate()
+        if "save_datasets" in config and config["save_datasets"]:
+            trainer.write_datasets_to_file()
+
+    elif operation == "evaluate":
+        pass  # @TODO: figure out what this entails
+
+    elif operation == "analyze":
+        if not model:
+            model = initialize_model(configs["model"], train=False)
+
+        analyze_seqs_info = configs["analyze_sequences"]
+        analyze_seqs_info.bind(model=model)
+
+        analyze_seqs = instantiate(analyze_seqs_info)
+
+        if "variant_effect_prediction" in configs:
+            vareff_info = configs["variant_effect_prediction"]
+            for filepath in vareff_info.pop("vcf_files"):
+                analyze_seqs.variant_effect_prediction(
+                    filepath, **vareff_info)
+
 if __name__ == "__main__":
     arguments = docopt(
         __doc__,
         version="1.0")
-    lr = float(arguments["<lr>"])
 
     configs = load_path(arguments["<config-yml>"], instantiate=False)
+    lr = arguments["--lr"]
 
-    ##################################################
-    # TRAIN MODEL PARAMETERS
-    ##################################################
-    model_info = configs["model"]
-    sampler_info = configs["sampler"]
-    model_controller_info = configs["model_controller"]
+    operations = configs.pop("ops")
 
-    sampler = instantiate(sampler_info)
+    if "lr" not in configs and lr != "None":
+        configs["lr"] = float(arguments["--lr"])
+    elif "lr" in configs and lr != "None" and "train" in operations:
+        print("Warning: learning rate specified in both {0} "
+              "and on the command line (--lr={1}). Using the command "
+              "line value for training.".format(
+                  arguments["<config-yml>"], lr))
 
+    # @TODO: allow users to pass in a random seed, optional
     torch.manual_seed(1337)
     torch.cuda.manual_seed_all(1337)
 
-    import_model_from = model_info["import_model_from"]
-    model_class_name = model_info["class"]
-    use_module = importlib.import_module(import_model_from)
-    model_class = getattr(use_module, model_class_name)
-
-    model = model_class(sampler.sequence_length, sampler.n_features)
-    print(model)
-
-    if model_info["non_strand_specific"]["use_module"]:
-        from models.non_strand_specific_module import NonStrandSpecific
-        model = NonStrandSpecific(
-            model, mode=model_info["non_strand_specific"]["mode"])
-
-    checkpoint_info = model_controller_info.pop("checkpoint")
-    checkpoint_resume = checkpoint_info.pop("resume")
-    checkpoint = None
-    if checkpoint_resume:
-        checkpoint_file = checkpoint_info.pop("model_file")
-        print("Resuming training from checkpoint {0}.".format(
-            checkpoint_file))
-        checkpoint = torch.load(checkpoint_file)
-        model.load_state_dict(checkpoint["state_dict"])
-        model.eval()
-    model_controller_info.bind(checkpoint_resume=checkpoint)
-
-    criterion = use_module.criterion()
-    optimizer_class, optimizer_args = use_module.get_optimizer(lr)
-
-    batch_size = model_controller_info.keywords["batch_size"] # Would love to find a better way.
-    max_steps = model_controller_info.keywords["max_steps"]
-    report_stats_every_n_steps = \
-        model_controller_info.keywords["report_stats_every_n_steps"]
-    n_validation_samples = model_controller_info.keywords["n_validation_samples"]
-
-    model_controller_info.bind(model=model,
-                               data_sampler=sampler,
-                               loss_criterion=criterion,
-                               optimizer_class=optimizer_class,
-                               optimizer_args=optimizer_args)
-    if "optional_args" in model_controller_info.keywords:
-        optional_args = model_controller_info.pop("optional_args")
-        model_controller_info.bind(**optional_args)
-    runner = instantiate(model_controller_info)
-
-    print("Training model: {0} steps, {1} batch size.".format(
-        max_steps, batch_size))
-    runner.train_and_validate()
-    if configs["evaluate_on_test"]:
-        runner.evaluate()
-    if configs["save_datasets"]:
-        runner.write_datasets_to_file()
+    for op in operations:
+        execute(op, configs)
