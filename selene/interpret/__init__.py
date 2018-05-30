b69b911636dd3a6ba2fb89f1136b8935c46b7b9c
evancofer <evancofer@gmail.com>
2018-05-30 15:50:55 -0400

    Renamed ism_results to ism_result to match class name

--- selene/interpret/__init__.py
+++ selene/interpret/__init__.py
@@ -6,6 +6,6 @@ trained with Selene.
 from .vis import sequence_logo
 from .vis import heatmap
 from .vis import rescale_score_matrix
-from .ism_results import ISMResult
+from .ism_result import ISMResult
 
 __all__ = ["sequence_logo", "heatmap", "rescale_score_matrix", "ISMResult"]
