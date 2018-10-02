8262f4d24f0af1a2e691a77246bd015041ae560d
Kathy <kchen2013@gmail.com>
2018-10-02 10:44:20 -0400

    data download script for case1

---
+++ manuscript/case1/download_data.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# All the data files needed for both training/evaluation and _in silico_
+# mutagenesis.
+
+wget https://zenodo.org/record/1442434/files/data.tar.gz
+
+tar -xzvf data.tar.gz
+
+# Outputs from training (e.g. log files, test performance, figures)
+
+wget https://zenodo.org/record/1442434/files/training_outputs.tar.gz
+
+tar -xzvf training_outputs.tar.gz
+
+# Outputs from _in silico_ mutagenesis (e.g. logit scores)
+
+wget https://zenodo.org/record/1442434/files/ism_outputs.tar.gz
+
+tar -xzvf ism_outputs.tar.gz
+
