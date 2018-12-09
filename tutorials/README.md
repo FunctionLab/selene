927cbe38ef18f2cf85223868aa46a80d636158b4
evancofer <evancofer@gmail.com>
2018-12-08 21:52:53 -0500

    Added a tutorial for regression with Selene

--- tutorials/README.md
+++ tutorials/README.md
@@ -8,7 +8,8 @@ To get started on training a model very quickly, please see [`quickstart_trainin
 Additionally, we have two tutorials that show how to apply trained models. Selene provides methods to run variant effect prediction and _in silico_ mutagenesis, along with some visualization methods that we recommend running based on our Jupyter notebook tutorials.
 
 - Comprehensive _in silico_ mutagenesis tutorial: [`analyzing_mutations_with_trained_models`](https://github.com/FunctionLab/selene/tree/master/tutorials/analyzing_mutations_with_trained_models)
-- Tutorial with both the config file method and the non-config file method of running Selene. Also shows how to run variant effect prediction and visualize the difference scores. Contains an _in silico_ mutagenesis example with known regulatory mutations: [`variants_and_visualizations`](https://github.com/FunctionLab/selene/tree/master/tutorials/variants_and_visualizations) 
+- Tutorial with both the config file method and the non-config file method of running Selene. Also shows how to run variant effect prediction and visualize the difference scores. Contains an _in silico_ mutagenesis example with known regulatory mutations: [`variants_and_visualizations`](https://github.com/FunctionLab/selene/tree/master/tutorials/variants_and_visualizations)
+- Tutorial demonstrating Selene's use to predict mean ribosomal load based on 5' UTR sequences: [`regression_mpra_example`](https://github.com/FunctionLab/selene/tree/master/tutorials/regression_mpra_example)
 
 ## Contributing tutorials
 
---
+++ tutorials/regression_mpra_example/utr_model.py
@@ -0,0 +1,64 @@
+"""
+Model derived from "Human 5′ UTR design and variant effect prediction from a massively parallel translation assay", https://doi.org/10.1101/310375
+"""
+
+import torch
+import numpy
+import torch.nn as nn
+
+
+class UTRModel(nn.Module):
+    def __init__(self, sequence_length=50, n_targets=1):
+        super(UTRModel, self).__init__()
+        self.sequence_length = sequence_length
+        kernel_size = 9 # Slight modification from model used in manuscript.
+        n_filters = 120
+        nodes = 40
+        padding = 4 # Note that this will be slightly different from original model's "same" padding in Keras.
+        self.cnn = nn.Sequential(
+                nn.Conv1d(4, n_filters, kernel_size=kernel_size, padding=padding),
+                nn.ReLU(inplace=True),
+                nn.Conv1d(n_filters, n_filters, kernel_size=kernel_size, padding=padding),
+                nn.ReLU(inplace=True),
+                nn.Conv1d(n_filters, n_filters, kernel_size=kernel_size, padding=padding),
+                nn.ReLU(inplace=True))
+        with torch.no_grad():
+            tmp = torch.zeros(1, 4, self.sequence_length)
+            dnn_input_size = self.cnn.forward(tmp).view(1, -1).shape[1]
+            del tmp
+        self.dnn = nn.Sequential(nn.Linear(dnn_input_size, nodes),
+                                 nn.ReLU(inplace=True),
+                                 nn.Dropout(0.20),
+                                 nn.Linear(nodes, n_targets))
+        # Copy weight initialization from Keras.
+        def init_weight(x):
+            if isinstance(x, nn.Linear) or isinstance(x, nn.Conv1d):
+                if isinstance(x, nn.Linear):
+                    fan_avg = (x.in_features + x.out_features) * 0.5
+                else:
+                    fan_avg = (x.weight.shape[0] + x.weight.shape[1]) * x.weight.shape[2] * 0.5
+                limit = numpy.sqrt(3 / fan_avg)
+                nn.init.uniform_(x.weight, -1 * limit, limit)
+                x.bias.data.fill_(0)
+        self.cnn.apply(init_weight)
+        self.dnn.apply(init_weight)
+
+    def forward(self, input):
+        batch_size = input.shape[0]
+        ret = self.dnn.forward(self.cnn.forward(input).view(batch_size, -1))
+        return ret
+
+
+def criterion():
+    """
+    The loss function to be optimized.
+    """
+    return nn.MSELoss(size_average=True, reduce=True)
+
+
+def get_optimizer(lr):
+    """
+    The optimizer and parameters.
+    """
+    return (torch.optim.Adam, {"lr": lr, "betas": (0.9, 0.999), "eps": 1e-08})
+
