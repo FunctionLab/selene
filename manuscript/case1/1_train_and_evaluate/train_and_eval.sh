e96f9443a0e67b767ae0c5d27a3bd4204c5e7582
Kathy <kchen2013@gmail.com>
2018-12-11 12:42:12 -0500

    update case 1 training parameters

--- manuscript/case1/1_train_and_evaluate/train_and_eval.sh
+++ manuscript/case1/1_train_and_evaluate/train_and_eval.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=1-02:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --gres=gpu:2
 #SBATCH --partition=gpu
 #SBATCH -n 1
@@ -7,7 +7,7 @@
 #SBATCH -o train_%j.out
 #SBATCH -e train_%j.err
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=kchen@flatironinstitute.org
+#SBATCH --mail-user=<EMAIL>
 
 source activate selene-env
 
