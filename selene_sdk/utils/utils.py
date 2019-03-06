4f16c2b4813a77782ebe7f71f6360a9e94ecec10
Kathy <kchen2013@gmail.com>
2019-03-06 16:48:58 -0500

    remove key checking in utils for loading state dict

--- selene_sdk/utils/utils.py
+++ selene_sdk/utils/utils.py
@@ -79,18 +79,6 @@ def load_model_from_state_dict(state_dict, model):
     for (k1, k2) in zip(model_keys, state_dict_keys):
         value = state_dict[k2]
         new_state_dict[k1] = value
-        """
-        elif ('model' in k1 and k2 in k1) \
-                or ('model' in k2 and k1 in k2):
-            new_state_dict[k1] = value
-        else:
-            raise ValueError("Model state dict keys do not match "
-                             "the keys specified in `state_dict` input. "
-                             "Cannot load state into the model:\n\n"
-                             "\tExpected keys:\n\t{0}\n\n"
-                             "\tKeys in the input state dict:\n\t{1}\n".format(
-                                 model_keys, state_dict_keys))
-        """
     model.load_state_dict(new_state_dict)
     return model
 
