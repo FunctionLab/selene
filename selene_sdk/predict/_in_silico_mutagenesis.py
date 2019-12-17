1bb78d45af634cacdec608f23da310b4aeb1ee1b
evancofer <evancofer@gmail.com>
2019-12-17 15:34:20 -0500

    Cli doc on selective ISM and better errors

--- selene_sdk/predict/_in_silico_mutagenesis.py
+++ selene_sdk/predict/_in_silico_mutagenesis.py
@@ -66,17 +66,27 @@ def in_silico_mutagenesis_sequences(sequence,
     if end_position is None:
         end_position = len(sequence)
     if start_position >= end_position:
-        raise ValueError("Starting positions must be less than the ending positions.")
+        raise ValueError(("Starting positions must be less than the ending "
+                          "positions. Found a starting position of {0} with "
+                          "an ending position of {1}.").format(start_position,
+                                                               end_position))
     if start_position < 0:
         raise ValueError("Negative starting positions are not supported.")
     if end_position < 0:
         raise ValueError("Negative ending positions are not supported.")
     if start_position >= len(sequence):
-        raise ValueError("Starting positions must be less than the sequence length.")
+        raise ValueError(("Starting positions must be less than the sequence length."
+                          " Found a starting position of {0} with a sequence length "
+                          "of {1}.").format(start_position, len(sequence)))
     if end_position > len(sequence):
-        raise ValueError("Ending positions must be less than or equal to the sequence length.")
+        raise ValueError(("Ending positions must be less than or equal to the sequence "
+                          "length. Found an ending position of {0} with a sequence "
+                          "length of {1}.").format(end_position, len(sequence)))
     if (end_position - start_position) < mutate_n_bases:
-        raise ValueError("Fewer bases exist in the substring specified by the starting and ending positions than need to be mutated.")
+        raise ValueError(("Fewer bases exist in the substring specified by the starting "
+                          "and ending positions than need to be mutated. There are only "
+                          "{0} currently, but {1} bases must be mutated at a "
+                          "time").format(end_position - start_position, mutate_n_bases))
 
     sequence_alts = []
     for index, ref in enumerate(sequence):
--- selene_sdk/predict/model_predict.py
+++ selene_sdk/predict/model_predict.py
@@ -710,18 +710,27 @@ class AnalyzeSequences(object):
         if end_position is None:
             end_position = self.sequence_length
         if start_position >= end_position:
-            raise ValueError("Starting positions must be less than the ending positions.")
+            raise ValueError(("Starting positions must be less than the ending "
+                              "positions. Found a starting position of {0} with "
+                              "an ending position of {1}.").format(start_position,
+                                                                   end_position))
         if start_position < 0:
             raise ValueError("Negative starting positions are not supported.")
         if end_position < 0:
             raise ValueError("Negative ending positions are not supported.")
         if start_position >= self.sequence_length:
-            raise ValueError("Starting positions must be less than the sequence length.")
+            raise ValueError(("Starting positions must be less than the sequence length."
+                              " Found a starting position of {0} with a sequence length "
+                              "of {1}.").format(start_position, self.sequence_length))
         if end_position > self.sequence_length:
-            raise ValueError("Ending positions must be less than or equal to the sequence length.")
+            raise ValueError(("Ending positions must be less than or equal to the sequence "
+                              "length. Found an ending position of {0} with a sequence "
+                              "length of {1}.").format(end_position, self.sequence_length))
         if (end_position - start_position) < mutate_n_bases:
-            raise ValueError("Fewer bases exist in the substring specified by the starting and ending positions than need to be mutated.")
-
+            raise ValueError(("Fewer bases exist in the substring specified by the starting "
+                              "and ending positions than need to be mutated. There are only "
+                              "{0} currently, but {1} bases must be mutated at a "
+                              "time").format(end_position - start_position, mutate_n_bases))
 
         path_dirs, _ = os.path.split(output_path_prefix)
         if path_dirs:
@@ -856,19 +865,27 @@ class AnalyzeSequences(object):
         if end_position is None:
             end_position = self.sequence_length
         if start_position >= end_position:
-            raise ValueError("Starting positions must be less than the ending positions.")
+            raise ValueError(("Starting positions must be less than the ending "
+                              "positions. Found a starting position of {0} with "
+                              "an ending position of {1}.").format(start_position,
+                                                                   end_position))
         if start_position < 0:
             raise ValueError("Negative starting positions are not supported.")
         if end_position < 0:
             raise ValueError("Negative ending positions are not supported.")
         if start_position >= self.sequence_length:
-            raise ValueError("Starting positions must be less than the sequence length.")
+            raise ValueError(("Starting positions must be less than the sequence length."
+                              " Found a starting position of {0} with a sequence length "
+                              "of {1}.").format(start_position, self.sequence_length))
         if end_position > self.sequence_length:
-            raise ValueError("Ending positions must be less than or equal to the sequence length.")
+            raise ValueError(("Ending positions must be less than or equal to the sequence "
+                              "length. Found an ending position of {0} with a sequence "
+                              "length of {1}.").format(end_position, self.sequence_length))
         if (end_position - start_position) < mutate_n_bases:
-            raise ValueError("Fewer bases exist in the substring specified by the starting and ending positions than need to be mutated.")
-
-
+            raise ValueError(("Fewer bases exist in the substring specified by the starting "
+                              "and ending positions than need to be mutated. There are only "
+                              "{0} currently, but {1} bases must be mutated at a "
+                              "time").format(end_position - start_position, mutate_n_bases))
 
         os.makedirs(output_dir, exist_ok=True)
 
