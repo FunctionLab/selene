2f1ca45334c54a5b68738fccb3252e149b7db06b
Kathy <kchen2013@gmail.com>
2020-05-10 13:44:03 -0400

    update version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ ext_modules = [genome_module, genomic_features_module]
 cmdclass = {'build_ext': build_ext}
 
 setup(name="selene-sdk",
-      version="0.4.7",
+      version="0.4.8",
       long_description=long_description,
       long_description_content_type='text/markdown',
       description=("framework for developing sequence-level "
