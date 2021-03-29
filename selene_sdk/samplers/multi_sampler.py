20713827174af6f570658651b6a801a6143832e4
Jian Zhou <jzthree@princeton.edu>
2021-03-29 14:48:06 -0400

    add doc for the deprecated MultiFileSampler

--- selene_sdk/samplers/multi_sampler.py
+++ selene_sdk/samplers/multi_sampler.py
@@ -9,6 +9,13 @@ from torch.utils.data import DataLoader
 from .sampler import Sampler
 
 def MultiFileSampler(*args, **kwargs):
+    """
+    `MultiFileSampler` is deprecated and will be removed from future
+    versions of Selene. Please use `MultiSampler` instead. This function
+    maintains backward compatibility for code that uses `MultiFileSampler`,
+    but we will remove this function in future. Please refer to the
+    `MultiSampler` documentation for usage.
+    """
     from warnings import warn
     warn("MultiFileSampler is deprecated and will be removed from future "
     "versions of Selene. Please use MultiSampler instead.")
