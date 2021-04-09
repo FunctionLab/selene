e71b79d9877c5dc60d1c798182798948ee922bd6
Flatiron Jenkins <jenkins@flatironinstitute.org>
2021-04-09 12:37:32 -0400

    Generated documentation
    jenkins-selene-master-46 601d0d11e11838a61778b4282482cacd20ce1a4a

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master',
+    VERSION: '0.5.dev0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
